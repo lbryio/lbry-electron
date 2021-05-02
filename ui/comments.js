Eniamza/lbry-desktop

Commentron Settings API

ui/comments.js
@@ -10,6 +10,10 @@ const Comments = {
   moderation_block_list: (params: ModerationBlockParams) => fetchCommentsApi('moderation.BlockedList', params),
   comment_list: (params: CommentListParams) => fetchCommentsApi('comment.List', params),
   comment_abandon: (params: CommentAbandonParams) => fetchCommentsApi('comment.Abandon', params),
+  setting_list: (params: SettingParams) => fetchCommentsApi('setting.List', params),
+  setting_block_word: (params: BlockWordParams) => fetchCommentsApi('setting.BlockWord', params),
+  setting_unblock_word: (params: BlockWordParams) => fetchCommentsApi('setting.UnBlockWord', params),
+  setting_list_blocked_words: (params: SettingParams) => fetchCommentsApi('setting.ListBlockedWords', params),
 };
 
 function fetchCommentsApi(method: string, params: {}) {

lbryio/lbry-electron

ui/comments.js
@@ -11,6 +11,11 @@ const Comments = {
   comment_list: (params: CommentListParams) => fetchCommentsApi('comment.List', params),
   comment_abandon: (params: CommentAbandonParams) => fetchCommentsApi('comment.Abandon', params),
   comment_create: (params: CommentCreateParams) => fetchCommentsApi('comment.Create', params),
+  setting_list: (params: SettingsParams) => fetchCommentsApi('setting.List', params),
+  setting_block_word: (params: BlockWordParams) => fetchCommentsApi('setting.BlockWord', params),
+  setting_unblock_word: (params: BlockWordParams) => fetchCommentsApi('setting.UnBlockWord', params),
+  setting_list_blocked_words: (params: SettingsParams) => fetchCommentsApi('setting.ListBlockedWords', params),
+  setting_update: (params: UpdateSettingsParams) => fetchCommentsApi('setting.Update', params),
   super_list: (params: SuperListParams) => fetchCommentsApi('comment.SuperChatList', params),
 };
 